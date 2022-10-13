a9702ea1002f48c1bdb4d18aee007d44efc007b9
Marvyn95 <marvinluyombya@gmail.com>
2022-10-13 13:22:16 -0700

    0-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main is the entry point to any c code
+ * and this is it
+ */
+
+/**
+ *my main function for the source code 
+ */
+int main(void)
+{
+	/**
+	 *this commands prints to the stdout
+	 *right\?
+	 */
+	puts("\"Programming is like building a multilingual puzzle")	
+	/**
+	 * My return value for the main function
+	 */
+	return (0)
+}
