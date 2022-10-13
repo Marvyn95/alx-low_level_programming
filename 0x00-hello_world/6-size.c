ee68645e295afb150cbd2112f66d47c329c7fcb6
Marvyn95 <marvinluyombya@gmail.com>
2022-10-13 14:32:41 -0700

    new task done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point into my source code
+ * return: 0 if successful 
+ * prints sizes of various types
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char)
+	printf("Size of an int: %d bytes(s)\n"), sizeof(int)
+	printf("Size of a long int: %d bytes(s)\n"), sizeof(long int)
+	printf("Size of a long long int: %d bytes(s)\n"), sizeof(long long int)
+	printf("Size of an float: %d bytes(s)\n"), sizeof(float)
+	return(0)
+}
