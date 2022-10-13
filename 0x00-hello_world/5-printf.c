080759e91b12860b2e41b70d7a4acdcb16846458
Marvyn95 <marvinluyombya@gmail.com>
2022-10-13 15:10:47 -0700

    new task done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/**main is  entry point for the program
- *prints words to standardoutput
- * Return:0 if errors arent present
- *
+/**
+ * main is  entry point for the program
+ * 
+ * prints words to standard output
+ * 
+ * Return: zero if errors arent present
  */
-
 int main(void)
 {
-	Printf("with proper grammer, but the outcome is a piece of art")
-	return (0)
+	Printf("with proper grammer, but the outcome is a piece of art");
+	return (0);
 }
