9bb887e9f698f3750d256f05bd16449d00d3ff66
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 16:41:57 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio>
+/**
+ * main -prints combination  which is smallest
+ * Return: zero if successful
+ */
+
+int main(void)
+{
+int a;
+
+int b;
+
+for (a = '0'; a <= '9'; a++)
+	for (b = a + 1; b <= '9'; b++)
+	{
+	putchar(a);
+	putchar(b);
+		if (a < '8')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+return (0);
+}
