609f2ced8fd477f91e47467fb40481c5f24051c5
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 13:42:37 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stsdio.h>
+/**
+ * main - prints entire alphabet but a few
+ * Return: zero if thumbs up
+ */
+
+int main(void)
+{
+	char chl;
+
+	for (chl = 'a'; chl <= 'z'; chl++)
+	{
+		if (chl == e || ch1 == q)
+		{
+			continue;
+		}
+	putchar(ch1);
+	}
+	putchar('\n');
+	return (0);
+}
