9f6c9229a6d9c62b272a31f1fb2dd3840d89f352
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 14:30:58 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints reverse alphabet in upper and lower case
+ * Return: returns zero if runs well
+ */
+
+int main(void)
+{
+	char ltl;
+
+	char ltu;
+
+	for (ltl = 'z'; ltl >= 'a'; ltl--)
+	{
+		putchar(ltl);
+	}
+}	for (ltu = 'Z'; ltu >= 'A'; ltu--)
+	{
+		putchar(ltu);
+	}
+	putchar('\n');
+	return (0);
