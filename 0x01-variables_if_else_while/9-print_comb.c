2b0714853784ac7f67d7611c491a9b5ab0b923e7
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 16:15:25 -0700

    C.c lets go

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 
 	for (k = '0'; k <= '9'; k++)
 	{
-		putchar(k);
-		if (k < 9)
+	putchar(k);
+		if (k < '9')
 		{
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+		putchar(' ');
 		}
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
