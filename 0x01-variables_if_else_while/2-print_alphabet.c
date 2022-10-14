7d432f7c97c0f5dc23ddbb770700dd0f2a4a9576
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 13:08:13 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - prints the alphabet
+ * Return: zero if all is well
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = a; ch <= z; ch++)
+	{
+	putchar(ch);
+	}
+	return (0);
+}
