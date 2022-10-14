92cdfc63a7f883e428015e8e37f21b543a6a0714
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 14:05:10 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints out one to nine
+ * Return: zero is amazing
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
