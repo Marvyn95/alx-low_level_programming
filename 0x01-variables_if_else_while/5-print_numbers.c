4dc995485bcaaf725a5c2414113f772896bda7f4
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 13:53:09 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints number, a few
+ * Return: zero is all is good
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0, n <= 9, n++)
+	{
+		printf("d%", n);
+	}
+	printf('\n');
+	return (0);
+}
