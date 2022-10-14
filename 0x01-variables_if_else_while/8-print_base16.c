22f8613fafdfd38613fd3d28a0f7ef5e6e2aecaa
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 14:50:55 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints numbers in base sixteen
+ * Return: returns zero if it works
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = '0'; b < '10'; b++)
+	{
+		putchar(b);
+	}
+	for (b = 'A'; b <= 'F'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
