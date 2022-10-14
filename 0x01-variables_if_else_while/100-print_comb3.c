0be68b30ab96b53239f23ce204c022e036631c28
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 16:58:31 -0700

    C.c lets go

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,24 @@
 
 int main(void)
 {
-int a;
+	int a;
 
-int b;
+	int b;
 
-for (a = '0'; a <= '9'; a++)
-{
-	for (b = a + '1'; b <= '9'; b++)
+	for (a = '0'; a <= '9'; a++)
 	{
-	putchar(a);
-	putchar(b);
-		if (a < '8')
+		for (b = a + '1'; b <= '9'; b++)
 		{
+			putchar(a);
+			putchar(b);
+			
+	if (a < '8')
+	{
 		putchar(',');
 		putchar(' ');
+	}	
 		}
-	}
 	putchar('\n');
-return (0);
+	return (0);
+	}
 }
