f29469b6719e5c4a82d77a925392866a18098d72
Marvyn95 <marvinluyombya@gmail.com>
2022-10-14 13:26:54 -0700

    C.c lets go

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in upper and lower case
+ * Return: zero if kawa
+ */
+
+int main(void)
+{
+	char chl;
+
+	char chu;
+
+	for (chl = 'a'; chl <= 'z'; chl++)
+	{
+	putchar(chl);
+	}
+	for (chu = 'A'; chu <= 'Z'; chu++)
+	{
+	putchar(chu);
+	}
+	return (0);
+}
