ca83329ea286cf93cdb312b7bdca16e329f24468
Marvyn95 <marvinluyombya@gmail.com>
2022-10-15 05:06:22 -0700

    C.c lets go

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
-
 /**
- * main - prints 2 digit combinations up to 99
- * Return: zero if successfully
+ * main - prints combinations of two digit combinations
+ * Return: zero if successful
  */
-
 int main(void)
 {
-	int a, b;
-
-
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 1; d <= 9; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (a != 9 && b != 9 && c != 9 && d != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+		putchar('\n');
+	}
+	return (0);
 }
