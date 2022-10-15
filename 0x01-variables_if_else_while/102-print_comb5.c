c0660b8f6ad94529289992ab5fee37930411df8d
Marvyn95 <marvinluyombya@gmail.com>
2022-10-15 14:06:55 -0700

    C.c lets go

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
-			putchat((a % 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
