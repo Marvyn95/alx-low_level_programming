be79e8f4de700f53e7269e4803e4c371338a783b
Marvyn95 <marvinluyombya@gmail.com>
2022-10-15 14:03:11 -0700

    C.c lets go

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,37 +5,23 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 99; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			for (c = 0; c <= 9; c++)
+			putchar((a / 10) + '0');
+			putchat((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
 			{
-				for (d = 1; d <= 9; d++)
-				{
-					if ((a == c && b == d) || (a ==d && b == c))
-					{
-					continue;
-					}
-					else
-					putchar(a + '0');
-					putchar(b + '0');
-					putchar(' ');
-					putchar(c + '0');
-					putchar(d + '0');
-					if (a == 9 && b == 8 && c == 9 && d == 9)
-					{
-						continue;
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			continue;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
