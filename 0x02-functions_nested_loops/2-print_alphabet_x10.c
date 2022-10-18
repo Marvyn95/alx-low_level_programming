e423ca5ca3faa58ecf3037dfc4a612adedd15ca5
Marvyn95 <marvinluyombya@gmail.com>
2022-10-18 15:28:59 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,12 @@
  * print_alphabet_x10 - prints alphabet 10 times
  */
 void print_alphabet_x10(void)
-{
-	char alpha = 'a';
-	
+{	
 	int i = 0;
 
 	while(i < 10)
 	{
-		alpha = 'a';
+		char alpha = 'a';
 
 		while(alpha <= 'z')
 		{
