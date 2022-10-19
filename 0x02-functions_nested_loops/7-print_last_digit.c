02518cca3a686864b4ad55f14aa8b0d916ea95ef
Marvyn95 <marvinluyombya@gmail.com>
2022-10-19 00:32:19 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,17 +3,15 @@
  * print_last_digit - prints the last digit of a number
  * @n: input argument
  * Return: lats digit
- *
  */
-
 int print_last_digit(int n)
 {
 	int lastd = n % 10;
-	
+
 		if (lastd < 0)
 		{
 			lastd *= -1;
 		}
 		_putchar(lastd + '0');
-		return (0);
+		return (lastd);
 }
