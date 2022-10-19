c4c7affba13b73716a0311a060030201f8bd8156
Marvyn95 <marvinluyombya@gmail.com>
2022-10-19 09:10:02 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers to 98
+ * @k: input arguments
+ * Return: zero if successful
+ */
+
+void print_to_98(int k)
+{
+	if (k < 98)
+	{
+		while (k <= 98)
+		{
+			_putchar(k);
+			if (k != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			k++;
+		}
+	}
+	else if (k > 98)
+	{
+		while (k >= 98)
+		{
+			_putchar(k);
+			if (k != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			k++;
+		}
+	}
+	else if (k == 98)
+	{
+		_putchar(98);
+	}
+	_putchar('\n');
+}
