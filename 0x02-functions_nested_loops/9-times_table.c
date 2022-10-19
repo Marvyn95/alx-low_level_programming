6fa66a48ecebc95dd3a863833d0940932593bd2e
Marvyn95 <marvinluyombya@gmail.com>
2022-10-19 07:35:05 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,10 +10,23 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+		_putcha('0');
+		_putchar(',');
+		_putchar(' ');
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			_putchar(c);
+
+			if ((d / 10) > 0)
+			{
+				_putchar((c / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((c % 10) + '0');
+
 			if (b != 9)
 			{
 				_putchar(',');
