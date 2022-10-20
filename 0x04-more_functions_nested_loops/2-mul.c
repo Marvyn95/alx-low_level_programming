cc0afb72c072002d362063b662ee17b2a0294103
Marvyn95 <marvinluyombya@gmail.com>
2022-10-20 12:26:47 -0700

    more functions lets go

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 numbers
+ * @a: first argument
+ * @b: second argument
+ * Return: product of the two inputs
+ */
+
+int main(int a, int b)
+{
+	return (a * b);
+}
