1061b4c73c693ef897f007553f0985d33d078e1e
Marvyn95 <marvinluyombya@gmail.com>
2022-10-20 07:48:58 -0700

    C.c lets go

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-/**
-	i = 0;
 
-	while (i < 10)
-	{
-		purchar(i);
-	}
+	i = 0;
+/*
+*	while (i < 10)
+*	{
+*		purchar(i);
+*	}
 */
 	printf("Infinite loop avoided! \\o/\n");
 
