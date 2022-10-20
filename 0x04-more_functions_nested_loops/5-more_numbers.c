59e5c480fd6da291b877786a7302f5b72d3dd77e
Marvyn95 <marvinluyombya@gmail.com>
2022-10-20 13:07:13 -0700

    more functions lets go

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,11 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (b >= 10)
+			{
+				_putchar(b);
+			}
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
