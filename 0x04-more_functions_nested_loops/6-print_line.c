86f24c17871cae158a63ce9e1ae36b248272166b
Marvyn95 <marvinluyombya@gmail.com>
2022-10-20 13:49:23 -0700

    more functions lets go

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: number entered
+ */
+
+void print_line(int n)
+{
+	int ln;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (ln = 1; ln <= n; ++n)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
