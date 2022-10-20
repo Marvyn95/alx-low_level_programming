f0a65fcf85a22beb11871118fb0a288a0485f6a8
Marvyn95 <marvinluyombya@gmail.com>
2022-10-20 13:17:07 -0700

    more functions lets go

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = '1'; a <= '10'; a++)
+	for (a = 1; a <= 10; a++)
 	{
-		for (b = '0'; b <= '14'; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
 			{
