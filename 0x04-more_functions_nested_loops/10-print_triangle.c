42f48e71f0491dacb1b54c9f896a4e47133daf1e
Marvyn95 <marvinluyombya@gmail.com>
2022-10-20 15:25:36 -0700

    more functions lets go

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,7 +18,7 @@ void print_triangle(int size)
 	{
 		for (r = 0; r <= (size - 1); r++)
 		{
-			for (s = 0; s <= (size - 1); s++)
+			for (s = 0; s < (size - 1) - r; s++)
 			{
 				_putchar(' ');
 			}
