8fc6a31e42c96776bf667c0633567d0003af8434
Marvyn95 <marvinluyombya@gmail.com>
2022-10-27 01:00:38 -0700

    another task done for more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - -encodes a string using rot 13
+ * @inp: input string address
+ * Return: returns encrypted string
+ */
+
+char *rot13(char *inp)
+{
+	int i;
+
+	for (i = 0; inp[i] != '\0'; i++)
+	{
+		if ((inp[i] >= 'a' && inp[i] < 'n')
+		|| (inp[i] >= 'A' && inp[i] < 'N'))
+			{
+				inp[i] = inp[i] + 13;
+			}
+		else if ((inp[i] >= 'n' && inp[i] <= 'z')
+		|| (inp[i] >= 'N' && inp[i] <= 'Z'))
+			{
+				inp[i] = inp[i] - 13;
+			}
+	}
+	return (inp);
+}
