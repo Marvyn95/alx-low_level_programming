4336bdbcb93cc8a5065af981bd7cf42031530b80
Marvyn95 <marvinluyombya@gmail.com>
2022-10-31 08:25:27 -0700

    another task done for more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for a charcter in a string
+ * @s: input string
+ * @c: charcter to search for
+ * Return: first occurence of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+		{
+			return((s + a));
+		}
+	}
+	return ('\0');
+}
