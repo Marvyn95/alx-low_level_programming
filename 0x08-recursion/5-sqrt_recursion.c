f58b4c38db49ca1de828c360861c06ee64746e5f
Marvyn95 <marvinluyombya@gmail.com>
2022-11-02 09:57:50 -0700

    another task done for more pointers, arrays and strings

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - computes square root of a number
+ * @n: number whose sqrt is needed
+ * Return: sqrt of n
+ * @y: root number
+ */
+int comp(int n, int y);
+{
+	if (y * y == n)
+	{
+		return (y);
+	}
+	else if (y * y > n)
+	{
+		return (-1);
+	}
+	return (comp(n, y + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	return (comp(n, 1));
+}
