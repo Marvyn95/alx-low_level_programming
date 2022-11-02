43056a4c5d960a33d23641a8b3684cf1f79089b5
Marvyn95 <marvinluyombya@gmail.com>
2022-11-02 09:20:00 -0700

    another task done for more pointers, arrays and strings

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints Yth power of a number x
+ * @x: number
+ * @y: power
+ * Return: yth power of x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+		return (1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y <= 1)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
