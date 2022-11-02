87c7e9429dc9918d7f689c4f4f6dee4487159127
Marvyn95 <marvinluyombya@gmail.com>
2022-11-02 11:19:59 -0700

    another task done for more pointers, arrays and strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * wildcmp - checks if 2 strings are identical
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical and 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
