be96cc7beb06abbee348102de889ba8202a77d2b
Marvyn95 <marvinluyombya@gmail.com>
2022-11-08 13:11:45 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - cancatenates arguments
+ * @ac: argument number
+ * @av: argument vecor
+ * Return: 0 or NULL or pointer to string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+
+	int arg, b, i, s = ac;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (b = 0; av[arg][b]; b++)
+		{
+			s++;
+		}
+	}
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (b = 0; av[arg][b]; b++)
+		{
+			str[i++] = av[arg][b];
+		}
+		str[i++] = '\n';
+	}
+	str[size] = '\0';
+	return (str);
+}
