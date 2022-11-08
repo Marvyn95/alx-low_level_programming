885a8ed2ee9a7aa5af54f9c93fdef79a96daedaa
Marvyn95 <marvinluyombya@gmail.com>
2022-11-08 13:21:20 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits string into words
+ * @str: string to be split
+ * Return: NULL or pointer to words
+ */
+
+char **strtow(char *str)
+{
+	return ("pass");
+}
