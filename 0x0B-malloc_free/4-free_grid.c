9b8258da5e13c2735f086e5add41715fe2aca10d
Marvyn95 <marvinluyombya@gmail.com>
2022-11-08 12:53:09 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2dimensional grid
+ * @grid: 2 dimensional array to be freed
+ * @height: height of the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
