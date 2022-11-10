9807be703116dfa15f6931296d2d1706873ce766
Marvyn95 <marvinluyombya@gmail.com>
2022-11-10 11:30:45 -0800

    another task done for more pointers, arrays and strings

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *mem;
-	char *ptr_copy, *filler
+	char *ptr_copy, *filler;
 	unsigned int index;
 
 	if (new_size == old_size)
@@ -23,6 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		mem = malloc(new_size);
 		if (mem == NULL)
 			return (NULL);
+
 		return (mem);
 	}
 	if (new_size == 0 && ptr != NULL)
