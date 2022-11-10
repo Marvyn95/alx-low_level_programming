fbd76f58af8aed41a1c4ec4b1fcaca74a1a01bd0
Marvyn95 <marvinluyombya@gmail.com>
2022-11-10 11:05:17 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: first value
+ * @max: second value
+ * Return: NULL or pointer to the array created
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, index, size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+	{
+		array[index] = min++;
+	}
+	return (array);
+}
