4f7e96d4d99468f579056b8f3f467e6eedf25af6
Marvyn95 <marvinluyombya@gmail.com>
2022-11-10 10:49:12 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory
+ * @nmemb: number of elements
+ * @size: bytes of each member
+ * Return: NULL or pointer to memory allocated
+ */
+
+void _calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *filler;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+	for (index = 0; index < (size * nmemb); index++)
+	{
+		filler[index] = '\0'
+	}
+	return (mem);
+}
