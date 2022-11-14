b5cedd164a0d69826ad749af7e2a60567c1320d9
Marvyn95 <marvinluyombya@gmail.com>
2022-11-14 09:47:18 -0800

    another task done for more pointers, arrays and strings

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,11 @@
 #ifndef FUNCTION_LIKE_MACRO
 #define FUNCTION_LIKE_MACRO
 
+/**
+ * File: 3-function_like_macro.h
+ * Description: computes absolute value of a integer
+ */
+
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
