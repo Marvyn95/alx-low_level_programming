affc65e76d199cb8bf3c06f7c04bb13334df276b
Marvyn95 <marvinluyombya@gmail.com>
2022-11-14 22:30:32 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+intrduction to structures and typedef alx tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type for dog information
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
