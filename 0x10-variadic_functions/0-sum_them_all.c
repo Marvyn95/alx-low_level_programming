82096f819fc59dcfcbb502574bd537f1ab2e989b
Marvyn95 <marvinluyombya@gmail.com>
2022-11-17 12:14:25 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all arguments passed
+ * @n: number of integers passed
+ * Return: sum of al the arguments passed
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list number;
+	unsigned int i, sum = 0;
+
+	va_start(number, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(number, int);
+	}
+
+	va_end(number);
+
+	return (sum);
+}
