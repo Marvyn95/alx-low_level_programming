b54fb109d778c56970223f6a51d462b7bc591c33
Marvyn95 <marvinluyombya@gmail.com>
2022-11-26 11:36:39 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - finds number of elements in a linked list
+ * @h: pointer to head of the list
+ * @elements: counter for elements
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
