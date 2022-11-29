903da34e8446ded235bcebd905bf7b8336dca858
Marvyn95 <marvinluyombya@gmail.com>
2022-11-29 07:44:07 -0800

    another task done for more pointers, arrays and strings

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (copy == NULL)
 		return (-1);
 
-	if (inedx == 0)
+	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(copy);
