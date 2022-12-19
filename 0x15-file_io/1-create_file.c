eed51a1e5b5f6e91b5ec1ae8a8e3ad0011d09de8
Marvyn95 <marvinluyombya@gmail.com>
2022-12-18 23:30:51 -0800

    another task done for more pointers, arrays and strings

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_context != NULL)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
 			;
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-		return (0);
+		return (-1);
 
 	close(o);
 	return (1);
