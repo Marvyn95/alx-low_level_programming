94be2d868ce4d02dcbf40b152fbc3a5345626b14
Marvyn95 <marvinluyombya@gmail.com>
2022-12-18 22:58:13 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a textfile and prints it to the POSIX stdout
+ * @filename: pointer to file name
+ * @letters: number of letters to print out
+ * Return: NULL or number of bytesprinted out
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (0 == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(0);
+
+	return (w);
+}
