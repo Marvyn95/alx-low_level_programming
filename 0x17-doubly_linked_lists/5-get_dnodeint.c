3d8969d99731ebe62e20e9473fd60d43b0a161e6
Marvyn95 <marvinluyombya@gmail.com>
2023-01-12 09:23:51 -0800

    another task done for more pointers, arrays and strings

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (head == NULL)
 			return (NULL);
