bd15795d460d9f635ca773ab9026948fab54af3b
Marvyn95 <marvinluyombya@gmail.com>
2023-01-12 08:40:29 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds new node at the beginning of a list
+ * @head: pointer to the first node
+ * @n: integer data to addd to the new node
+ * Return: NULL or address of the new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	*(new).n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head != NULL)
+		*head.prev = new;
+	*head = new
+	return (new);
+}
