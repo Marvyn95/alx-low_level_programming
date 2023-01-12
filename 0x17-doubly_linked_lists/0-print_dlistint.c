9906caa80128fa7a9ddf7fa0581f11a1ce68a7ef
Marvyn95 <marvinluyombya@gmail.com>
2023-01-12 08:26:38 -0800

    another task done for more pointers, arrays and strings

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
-	dlistint_t *p = h;
 
-	while (p != NULL)
+	while (h != NULL)
 	{
 		nodes = nodes + 1;
-		printf("%d\n", p->n);
-		p = p->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (nodes);
 }
