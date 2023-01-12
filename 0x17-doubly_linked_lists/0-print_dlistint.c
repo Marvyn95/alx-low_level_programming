6f7af262b5e207bf3df8f130f7bd82405edb2c86
Marvyn95 <marvinluyombya@gmail.com>
2023-01-12 08:06:23 -0800

    another task done for more pointers, arrays and strings

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints elements of a doubly linked list
+ * @h: head of the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes = nodes + 1;
+		printf("%d\n", h->n);
+		h = h->next
+	}
+	return (nodes);
+}
